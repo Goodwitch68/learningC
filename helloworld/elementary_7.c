5f65aefda00a8f1600bdf1c44da6fa7d4d5993fe
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-16 17:03:09 +0200

    Elementary_7 working, before adding is_prime().

--- helloworld/elementary_7.c
+++ helloworld/elementary_7.c
@@ -4,7 +4,13 @@
 // először is korlátozzuk a számot, különben belehalunk a várakozásba, 
 // hiszen nagyon sok prim szám van még INT tartományban is
 
-int main(){
+/*int is_prime(int szam, int i) {
+	
+	
+	return prime
+}*/
+
+int main() {
     int szam;
 	int hatar;
     int i;
@@ -15,21 +21,20 @@ int main(){
 	scanf("%d", &hatar);
     
 	printf("2\n");
-	for(szam=3; szam <= hatar; szam = szam + 2)
-    {prime = 0;
-        for(i=2; i<=sqrt(szam+1); i++){            
-                if(szam % i ==0) // akkor nem prim, menj a következő osztóra
-				{prime = 1;
+	for(szam=3; szam <= hatar; szam = szam + 2) {
+		prime = 0;
+        for(i=2; i<=sqrt(szam+1); i++) {            
+			if(szam % i ==0) { // akkor nem prim, menj a következő osztóra
+				prime = 1;
 				break;
-				}
-				// minden más esetben menj a következő osztóra
-            
+			}
+			// minden más esetben menj a következő osztóra
 		}
-		if (prime = 0) {
+		if (prime == 0) {
 			setbuf(stdout, NULL);
 			printf("%d  %d\n", szam, prime); //kiíratom vele, hogy prim-e, hogy lássam, hol van a baj
-			}
+		}
 	}
 
-return 0;
+	return 0;
 }
