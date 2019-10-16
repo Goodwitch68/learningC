726d0855eeb1ccd22519175dd8a792bc8cbac8d3
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-16 16:11:21 +0200

    jól dönti el, hogy prim-e, de nem jól ír ki

--- helloworld/elementary_7.c
+++ helloworld/elementary_7.c
@@ -1,24 +1,35 @@
 #include<stdio.h>
 #include<math.h>
 
+// először is korlátozzuk a számot, különben belehalunk a várakozásba, 
+// hiszen nagyon sok prim szám van még INT tartományban is
+
 int main(){
-
     int szam;
+	int hatar;
     int i;
-    int darab=0;
-
-    for(szam=2;     ; szam++)
-    {
-        darab = 0;
-
-            for(i=1; i<=sqrt(szam); i++)
-            {
-                if(szam % i == 0){ darab++; }
-                if(darab>2){ break; }
-            }
-
-        if( darab == 1){ printf("%d\n", szam); }
-    }
+	int prime;
+	
+	setbuf(stdout, NULL);
+	printf("Meddig terjedjen a primek kiírása? ");
+	scanf("%d", &hatar);
+    
+	printf("2\n");
+	for(szam=3; szam <= hatar; szam = szam + 2)
+    {prime = 0;
+        for(i=2; i<=sqrt(szam+1); i++){            
+                if(szam % i ==0) // akkor nem prim, menj a következő osztóra
+				{prime = 1;
+				break;
+				}
+				// minden más esetben menj a következő osztóra
+            
+		}
+		//if (prime = 0) {
+			setbuf(stdout, NULL);
+			printf("%d  %d\n", szam, prime); //kiíratom vele, hogy prim-e, hogy lássam, hol van a baj
+			//}
+	}
 
 return 0;
 }
