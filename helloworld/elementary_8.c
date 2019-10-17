f235aa4ed15a00235c5798d10f0edc9c6af0aaef
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-17 15:10:24 +0200

    Elementary_8 working

---
+++ helloworld/elementary_8.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+// ez tképpen a 7-es feladat, csak benéztem a számot
+
+int main(){
+    
+    int i;
+	int j;
+	
+	//setbuf(stdout, NULL);
+	printf("A szorzótábla\n ");
+	    
+	for(i=1; i <= 12; i++){
+		setbuf(stdout, NULL);
+		printf("\nAz %d-s szorzótábla:\n", i);
+		
+        for(j=1; j<= 12; j++){            
+             printf("%d * %d = %d\n", j, i, i*j);   
+		}
+	}
+
+return 0;
+}
+
+//kész
