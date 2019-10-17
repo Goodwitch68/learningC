1859aeb5be6ab96eef95e8e66c50d3139dd79aee
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-17 15:05:13 +0200

    Elementary_6 working with bug in printf

--- helloworld/elementary_6.c
+++ helloworld/elementary_6.c
@@ -2,45 +2,39 @@
 
 int main() {
 
-    int szam;
+    int szam = 0;
 	int osszeg = 0;
-	int szorzat = 0;
-	char dontes 
+	int szorzat = 1;
+	char dontes = 'p';
   
   	setbuf(stdout, NULL);
 	
 	printf("Írj egy számot: ");    
 	scanf("%d", &szam);
-	
-	printf("A számok összegét (S) vagy szorzatát számoljam ki (P)?");    
-	scanf("%s", &dontes);
-	
-	if (dontes == 'S') {
 		
+	printf("A számok összegét (s) vagy szorzatát (p) számoljam ki 1-től n-ig? ");    
+	scanf("%c", &dontes);
+	printf("%c", dontes);
+	
+	while (dontes != 's' && dontes !='p') {
+		setbuf(stdout, NULL);
+		printf("Kérlek nyomj s-t vagy p-t!\n");
+		scanf("%c", &dontes);
+	}
+	if (dontes == 's') {
 		osszeg = (1 + szam)*szam/2; 
-		setbuf(stdout, NULL);
-		printf("A számok összege 1-%d-ig:%d", szam, osszeg);
+		printf("A számok összege 1-%d-ig:%d\n", szam, osszeg);
 	}
-	if (dontes == 'P') {
-		
-		szorzat = (1 + szam)*szam/2; 
-		setbuf(stdout, NULL);
-		printf("A számok összege 1-%d-ig:%d", szam, osszeg);
-		int i;
-	for (i=1; i <= szam; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0) {
-			
-			osszeg = osszeg + i;
-			
+	if (dontes == 'p') {
+		for (int i = 1; i <= szam; i++) {
+			szorzat = szorzat * i; 
 		}
+		printf("A számok produktuma 1-%d-ig:%d\n", szam, szorzat);
 	}
-		
-	
-	setbuf(stdout, NULL);
-	printf("A hárommal vagy öttel osztható számok összege 1-%d-ig:%d", szam, osszeg);
 	
   
 	return 0;
 }
-// kész
+//ebben is van még varázslat. Az előtt lefut a while ciklus,
+// mielőtt be olvashatná a dontes valtozót. 
+//Látszólag nem csinál hülyeséget a program, mert értelmes így is, de ez hiba
