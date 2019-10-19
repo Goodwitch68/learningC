8b359266ba5af9f911ef0228d6783f674f7b613c
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-19 11:05:56 +0200

    elementary_9 proper working

--- helloworld/elementary_9.c
+++ helloworld/elementary_9.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     a = rand() % 100 + 1;
    
 	int number;
+	int old_number = 0;
 	int trials = 0;
    
 	setbuf(stdout, NULL);
@@ -27,8 +28,12 @@ int main(int argc, char *argv[])
 			printf("Nem talált, a szám nagyobb, mint a Te tipped. Próbáld újra! \n");
 			scanf(" %d", &number);
 			}
+		if (old_number == number) {
+			trials--;
+			printf("ezt a számot épp most írtad, figyelj jobban!");
+			}
+		old_number = number;
 	}
 	printf("Szuper! Eltaláltad! %d-t találgattál.\n", trials);
 	return 0;
 }
-//Az ismétlések eliminálása még nincs benne
