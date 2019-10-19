dce717a3c9702939cc100f91a2840256c3e22af9
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-19 09:03:35 +0200

    Elementary_9 working, without eliminating of consecutiv repeatition

---
+++ helloworld/elementary_9.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a;
+    srand(time(NULL));
+    a = rand() % 100 + 1;
+   
+	int number;
+	int trials = 0;
+   
+	setbuf(stdout, NULL);
+	//printf(" %d", a);
+	
+	printf("\nGondoltam egy számra 1 és 100 között, találd ki melyikre!  \n");
+	scanf(" %d", &number);
+	trials++;
+	
+	while (number != a){
+		trials++;
+		if (number < a){
+			printf("Nem talált, a szám kisebb, mint a Te tipped. Próbáld újra! \n");
+			scanf(" %d", &number);
+			}
+		else {
+			printf("Nem talált, a szám nagyobb, mint a Te tipped. Próbáld újra! \n");
+			scanf(" %d", &number);
+			}
+	}
+	printf("Szuper! Eltaláltad! %d-t találgattál.\n", trials);
+	return 0;
+}
+//Az ismétlések eliminálása még nincs benne
