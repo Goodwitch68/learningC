c5a2e5f31b3486da82b6370b0aa8f75e87f37840
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-19 11:33:28 +0200

    elementary_9 better

--- helloworld/elementary_9.c
+++ helloworld/elementary_9.c
@@ -12,28 +12,31 @@ int main(int argc, char *argv[])
 	int trials = 0;
    
 	setbuf(stdout, NULL);
-	//printf(" %d", a);
-	
+	printf(" %d", a);
 	printf("\nGondoltam egy számra 1 és 100 között, találd ki melyikre!  \n");
 	scanf(" %d", &number);
 	trials++;
 	
 	while (number != a){
 		trials++;
-		if (number < a){
-			printf("Nem talált, a szám kisebb, mint a Te tipped. Próbáld újra! \n");
-			scanf(" %d", &number);
-			}
-		else {
-			printf("Nem talált, a szám nagyobb, mint a Te tipped. Próbáld újra! \n");
-			scanf(" %d", &number);
-			}
+		printf("régi szám:%d, új szám:%d", old_number, number);
 		if (old_number == number) {
 			trials--;
-			printf("ezt a számot épp most írtad, figyelj jobban!");
-			}
-		old_number = number;
+			printf("ezt a számot épp most írtad, figyelj jobban!\n");
+		}
+		if (number < a){
+			printf("Nem talált, a szám kisebb, mint a Te tipped. Próbáld újra! \n");
+			old_number = number;
+			scanf(" %d", &number);
+		}
+		else {
+			printf("Nem talált, a szám nagyobb, mint a Te tipped. Próbáld újra! \n");
+			old_number = number;
+			scanf(" %d", &number);
+		}
+		
 	}
 	printf("Szuper! Eltaláltad! %d-t találgattál.\n", trials);
 	return 0;
 }
+//most jó
