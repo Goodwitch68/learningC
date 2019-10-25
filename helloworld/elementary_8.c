e56181553dc3359b0f31fe8cfedfac83ae917176
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-25 12:15:24 +0200

    elementary_8 intended proper way

--- helloworld/elementary_8.c
+++ helloworld/elementary_8.c
@@ -4,19 +4,17 @@
 // ez tképpen a 7-es feladat, csak benéztem a számot
 
 int main(){
-    
-    int i;
+
+	int i;
 	int j;
 	
-	//setbuf(stdout, NULL);
 	printf("A szorzótábla\n ");
-	    
-	for(i=1; i <= 12; i++){
+	
+	for(i = 1; i <= 12; i++){
 		setbuf(stdout, NULL);
 		printf("\nAz %d-s szorzótábla:\n", i);
-		
-        for(j=1; j<= 12; j++){            
-             printf("%d * %d = %d\n", j, i, i*j);   
+		for(j = 1; j <= 12; j++){
+			printf("%d * %d = %d\n", j, i, i*j);
 		}
 	}
 
