95a5c877cd6ab110d2442e76e3ee08e30996dee5
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-25 12:28:58 +0200

    elementary_10 intended proper way

--- helloworld/elementary_10.c
+++ helloworld/elementary_10.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <time.h>
-int main()
-{
-    time_t s, val = 1;
-    struct tm* current_time;
-    
-    s = time(NULL);
-    
-    current_time = localtime(&s);
-    
-    printf("Current year = %d\n",(current_time->tm_year + 1900));
-    
-    
+int main() {
+
+	time_t s, val = 1;
+	struct tm* current_time;
+	
+	s = time(NULL);
+	
+	current_time = localtime(&s);
+	printf("Current year = %d\n",(current_time->tm_year + 1900));
 	int current_year = current_time->tm_year + 1900;
 	int next_leap_year = 4 - current_year % 4;
 	int leap_year = current_year + next_leap_year;
 	
-	for (int i = 0; i< 20; i++){
+	for (int i = 0; i < 20; i++){
 		if (leap_year % 100 == 0){
 			i--;
 		}
@@ -25,9 +22,6 @@ int main()
 		}
 		leap_year = leap_year + 4;
 	}
-    
-	
-	
 	return 0;
 }
 
