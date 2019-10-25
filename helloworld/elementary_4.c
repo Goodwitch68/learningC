4c30325010d5b288336d2ebbf53a4b27df10ab68
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-10-25 11:53:34 +0200

    elementary_4.c intended proper way

--- helloworld/elementary_4.c
+++ helloworld/elementary_4.c
@@ -2,20 +2,17 @@
 
 int main() {
 
-    int szam;
+	int szam;
 	int osszeg;
-  
-  	setbuf(stdout, NULL);
-	
-	printf("Írj egy számot: ");    
-	scanf("%d", &szam);
 
+	setbuf(stdout, NULL);
+	printf("Írj egy számot: ");
+	scanf("%d", &szam);
+	
 	osszeg = (1+szam)*szam/2; // persze lehetett volna nagyon idétlen ciklussal is megoldani, de ez elegánsabb megoldásnak tűnt :-)
-	//a 
-	//
 	setbuf(stdout, NULL);
 	printf("A számok összege 1-%d-ig:%d", szam, osszeg);
 	
-  
+
 	return 0;
 }
