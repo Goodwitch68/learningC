006e8e3bf802c382e2b0b3d38d4470064635cc59
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-11-04 12:08:23 +0100

    H14_E3 and 4 have problem with solving, pls help

--- helloworld/H15_E4.c
+++ helloworld/H15_E4.c
@@ -13,30 +13,26 @@ main () {
 	double d4 = 4.5;
 	
 	printf("Given an argument: %2.1f\n", d1);
-	printf("The result returned by AddDouble() is: %2.1f\n\n",
-	AddDouble(1, d1));
+	printf("The result returned by AddDouble() is: %2.1f\n\n", 	AddDouble(1, d1));
 	printf("Given arguments: %2.1f and %2.1f\n", d1, d2);
-	printf("The result returned by AddDouble() is: %2.1f\n\n",
-	AddDouble(2, d1, d2));
+	printf("The result returned by AddDouble() is: %2.1f\n\n", 	AddDouble(2, d1, d2));
 	printf("Given arguments: %2.1f, %2.1f and %2.1f\n", d1, d2, d3);
-	printf("The result returned by AddDouble() is: %2.1f\n\n",
-	AddDouble(3, d1, d2, d3));
+	printf("The result returned by AddDouble() is: %2.1f\n\n", 	AddDouble(3, d1, d2, d3));
 	printf("Given arguments: %2.1f, %2.1f, %2.1f, and %2.1f\n", d1, d2, d3, d4);
-	printf("The result returned by AddDouble() is: %2.1f\n",
-	AddDouble(4, d1, d2, d3, d4));
+	printf("The result returned by AddDouble() is: %2.1f\n", AddDouble(4, d1, d2, d3, d4));
 	return 0;
-	}
+}
 	/* definition of AddDouble() */
-	double AddDouble(int x, ...)
-	{
+double AddDouble(int x, ...) {
 	va_list arglist;
 	int i;
 	double result = 0.0;
-	
 	printf("The number of arguments is: %d\n", x);
 	va_start (arglist, x);
-	for (i=0; i<x; i++)
-	result += va_arg(arglist, double);
+	for (i=0; i<x; i++) result += va_arg(arglist, double);
 	va_end (arglist);
-	return result;
-	}
+return result;
+}
+	
+	//na ezzel ugyanaz a problémám, mint az előző feladattal. :-(
+	
