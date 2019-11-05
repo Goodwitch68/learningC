90f068dca0dd2452f5608c5f73aa4b4da3024cd5
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-11-05 13:27:50 +0100

    H17_E1 ready for checking

--- helloworld/H17_E1.c
+++ helloworld/H17_E1.c
@@ -2,63 +2,44 @@
 //to allocate. Then, initialize the allocated memory with consecutive integers, starting
 //from 1. Add all the integers contained by the memory block and print out the final
 //result on the screen.
-//2. Write a program that allocates a block of memory space to hold 100 items of the
-//float data type by calling the calloc() function. Then, reallocate the block of
-//memory in order to hold 50 more items of the float data type.
-//3. Write a program to ask the user to enter the total number of float data. Then use
-//the calloc() and malloc() functions to allocate two memory blocks with the
-//same size specified by the number, and print out the initial values of the two memory blocks.
-//4. Rewrite the program in Listing 17.4. This time, use the two special cases of the
-//realloc() function to replace the malloc() and free() functions.
 
- /* 17L04.c: Using the realloc() function */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /* function declaration */
-void StrCopy(char *str1, char *str2);
+int SumNumbers (int x);
+
 /* main() function */
+
 main() {
-	char *str[4] = {"There's music in the sighing of a reed;",
-	"There's music in the gushing of a rill;",
-	"There's music in all things if men had ears;",
-	"There earth is but an echo of the spheres.\n"
-	};
-	char *ptr;
-	int i;
-	
-	int termination = 0;
-	
-	ptr = malloc((strlen(str[0]) + 1) * sizeof(char));
-	if (ptr == NULL){
-	printf("malloc() failed.\n");
-	termination = 1;
+	int SizeofAllmemory;
+	int *ptr_x;
+	int sum = 0;
+	int result;
+	setbuf(stdout, NULL);
+	printf("Enter the total number of bytes you want to allocate:");
+	scanf("%d", &SizeofAllmemory);
+	ptr_x = malloc (SizeofAllmemory/2 * sizeof(int));
+	//ptr_x = malloc (SizeofAllmemory); Ez vajon miért nem jó helyette? segmentation faultot ad vissza
+	if (ptr_x != NULL){
+		for (int i = 0; i < SizeofAllmemory/2; i++){
+			ptr_x[i] = i + 1;
+			printf("%d\n", *(ptr_x + i));
+		}
+		printf("Az összeg az elején: %d\n", sum);
+		for (int i = 0; i < SizeofAllmemory/2; i++){
+			sum += ptr_x[i];
+			printf("Az összeg most: %d\n", sum);
+		}
+		printf("The sum is %d\n", sum);
+		result = 0;
 	}
-	else{
-	StrCopy(str[0], ptr);
-	printf("%s\n", ptr);
-	for (i=1; i<4; i++){
-	ptr = realloc(ptr, (strlen(str[i]) + 1) * sizeof(char));
-	if (ptr == NULL){
-	printf("realloc() failed.\n");
-	termination = 1;
-	i = 4; /* break the fro loop */
+	else {
+		printf("malloc() function failed.\n");
+		result = 1;
 	}
-	else{
-	StrCopy(str[i], ptr);
-	printf("%s\n", ptr);
-	}
-	}
-	}
-	free(ptr);
-	return termination;
-	}
-	/* funciton definition */
-	void StrCopy(char *str1, char *str2)
-	{
-	int i;
-	
-	for (i=0; str1[i]; i++)
-	str2[i] = str1[i];
-	str2[i] = ‘\0';
-	}
+	printf("%d", result);
+	return result;
+}
+
+
