41903885c93a776c3263581907678aaff103240e
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-11-05 14:11:03 +0100

    H14_E4 ready for checking

---
+++ helloworld/H17_E4.c
@@ -0,0 +1,54 @@
+//4. Rewrite the program in Listing 17.4. This time, use the two special cases of the
+//realloc() function to replace the malloc() and free() functions.
+
+ /* 17L04.c: Using the realloc() function */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* function declaration */
+void StrCopy(char *str1, char *str2);
+/* main() function */
+main() {
+	char *str[4] = {"There's music in the sighing of a reed;",
+	"There's music in the gushing of a rill;",
+	"There's music in all things if men had ears;",
+	"There earth is but an echo of the spheres.\n"
+	};
+	char *ptr;
+	int i;
+	
+	int termination = 0;
+	
+	ptr = realloc(NULL, (strlen(str[0]) + 1) * sizeof(char)); //ptr = malloc((strlen(str[0]) + 1) * sizeof(char));
+	if (ptr == NULL){
+	printf("malloc() failed.\n");
+	termination = 1;
+	}
+	else{
+	StrCopy(str[0], ptr);
+	printf("%s\n", ptr);
+	for (i=1; i<4; i++){
+	ptr = realloc(ptr, (strlen(str[i]) + 1) * sizeof(char));
+	if (ptr == NULL){
+	printf("realloc() failed.\n");
+	termination = 1;
+	i = 4; /* break the fro loop */
+	}
+	else{
+	StrCopy(str[i], ptr);
+	printf("%s\n", ptr);
+	}
+	}
+	}
+	realloc(ptr, 0); //free(ptr)
+	return termination;
+	}
+	/* funciton definition */
+	void StrCopy(char *str1, char *str2)
+	{
+	int i;
+	
+	for (i=0; str1[i]; i++)
+	str2[i] = str1[i];
+	str2[i] = '\0';
+	}
