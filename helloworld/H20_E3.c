42716ddf939a7bbbe4d44544724461dd54909f4b
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-11-06 15:19:50 +0100

    H20_E3 Pls check, i've problem in line 24. The evaluation has mistake.

--- helloworld/H20_E3.c
+++ helloworld/H20_E3.c
@@ -3,3 +3,34 @@
 //name of the state where he or she comes from. Otherwise, ask the user to enter the
 //name of the country he or she comes from. (You're required to use a union in your
 //program.)
+
+#include <stdio.h>
+#include <string.h>
+
+main() {
+	union citizens {
+	char name[40];
+	char citstat[4];
+	char state [20];
+	char country [30];
+	} user;
+
+	setbuf(stdout, NULL);
+	printf("Enter your name:");
+	gets (user.name);
+	printf ("Are you a U.S. citizen (Yes/No)");
+	gets (user.citstat);
+	printf ("%s\n", user.citstat);
+	int eval = user.citstat != "No";
+	printf ("%d\n", eval);
+	if (user.citstat != "No") {
+		printf("Enter the name of the state where you come from.");
+		scanf("%s", &user.state);
+	}
+	else {
+		printf("Enter the name of the country where you come from.");
+		scanf("%s", &user.country);
+	}
+
+	return 0;
+}
