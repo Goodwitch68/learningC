6f153a40730b7258c79eccef34f4a8e8e02e75bd
Goodwitch68 on AcerS7 <56366861+Goodwitch68@users.noreply.github.com>
2019-11-06 13:10:03 +0100

    H19_E1 ready for checking

--- helloworld/H19_E1.c
+++ helloworld/H19_E1.c
@@ -12,6 +12,21 @@ double weight;
 write a program to display on the screen the initial values held by the 
 structure.*/
 
+#include <stdio.h>
+
+main() {
+	struct automobile {
+		int year;
+		char model[10];
+		int engine_power;
+		double weight;
+	} sedan = { 1997, "New Model", 200, 2345.67};
+	
+	printf("%d, %s, %d, %8.2f", sedan.year, sedan.model, sedan.engine_power, sedan.weight);
+	
+	return 0;
+}
+
 
 
 
